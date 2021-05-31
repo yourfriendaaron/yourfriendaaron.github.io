1f7453bb855f2f094efbd90f874cf733bc628599
Aaron <aarontaylor8888@gmail.com>
2021-05-31 00:50:08 -0400

    changes theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Agency v7.0.0 (https://startbootstrap.com/theme/agency)
+* Start Bootstrap - Grayscale v7.0.0 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
 //
 // Scripts
